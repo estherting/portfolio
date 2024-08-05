95c2bef9b4a9fd2cf1a9a9bd25000c7c486c94e1
Esther Ting <31228765+estherting@users.noreply.github.com>
2024-08-05 15:00:24 -0700

    First commit

---
+++ src/main/java/com/portfolio/portfolio/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.portfolio.portfolio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
